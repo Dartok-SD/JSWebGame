46d66e57a18d124a7f1e3004c250447e91758140
David Sun <davidsun@umail.ucsb.edu>
2019-10-30 20:05:15 -0700

    Added new collision test/resolution

--- game.js
+++ game.js
@@ -56,43 +56,175 @@ function keyUpHandler(e) {
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var ground = {x:0, y:425, width: canvas.width, height: 30, mode:-1};
-var level = [ground,{x : 200, y:350, width: 90, height: 30, mode:-1},
-    {x:350,y:350,width:50,height:100, mode:-1}, {x:550, y:350,width:100,height:100, mode:-1}, {x:350,y:250,width:50,height:100,mode:0}];
+var STICKY_THRESHOLD = 0.0004
+var ground = {x:0, y:425, width: canvas.width, height: 30, mode:-1, restitution:0.2};
+var level = [ground,{x : 200, y:350, width: 90, height: 30, mode:-1, restitution:0.2},
+    {x:350,y:350,width:50,height:100, mode:-1, restitution:0.2}, {x:550, y:350,width:100,height:100, mode:-1, restitution:0.2},
+    {x:350,y:250,width:50,height:100,mode:0, restitution:0.2}];
 // var y = 400;
 // var x = 30;
 var dx = 0;
 var dy = 0;
 console.log("Game");
+// wallCollision();
+// var didCollide = groundCollision();
+// headCollision();
+// wallCollision();
+// if(!didCollide[0]){
+// } else {
+//     if(dy > 0){
+//         player.y = didCollide[1].y-player.width;
+//         notDoubleJumped = true;
+//     }
+// }
+// console.log("Dx",dx);
 function draw(){
     context.clearRect(0, 0, canvas.width, canvas.height);
     gravity();
-    dx = dxSpeed();
-    // wallCollision();
-    jump();
-    // var didCollide = groundCollision();
-    // headCollision();
-    // wallCollision();
-    capJump();
+    userInteraction();
+    collisionDetection();
+    // console.log("DY: " + dy);
     player.y+=dy;
-    // if(!didCollide[0]){
-    // } else {
-    //     if(dy > 0){
-    //         player.y = didCollide[1].y-player.width;
-    //         notDoubleJumped = true;
-    //     }
-    // }
-    // console.log("Dx",dx);
     player.x += dx;
-    changeMode();
+    if(grounded){
+        console.log("Grounded");
+    }
     drawGround();
     drawLevel();
     drawPlayer();
 }
 // Sometimes there is a bug that causes jump to be infinite
+function userInteraction(){
+    changeMode();
+    dx = dxSpeed();
+    jump();
+}
 function capJump(){
     if(dy<-15){
         dy = -15;
+    }
+}
+// function collisionDetection(l2,t2,r2,b2){
+//     var l1 = player
+// }
+function collisionDetection(){
+    for(let i = 0; i < level.length;i++){
+        if(collisionTest(player,level[i])){
+            // console.log("Collision Detected");
+            collisionResolution(level[i]);
+        }
+    }
+}
+function collisionTest(object1, object2){
+    var l1 = object1.x;
+    var t1 = object1.y;
+    var r1 = object1.x+object1.width;
+    var b1 = object1.y+object1.height;
+
+    var l2 = object2.x;
+    var t2 = object2.y;
+    var r2 = object2.x+object2.width;
+    var b2 = object2.y+object2.height;
+
+    return !(b1 < t2 || t1 > b2 || r1 < l2 || l1 > r2);
+
+}
+function collisionResolution(entity){
+    var pMidX = player.x + player.width/2;
+    var pMidY = player.y + player.height/2;
+    var aMidX = entity.x + entity.width/2;
+    var aMidY = entity.y + entity.height/2;
+
+    var normalX = (aMidX - pMidX)/(entity.width/2);
+    var normalY = (aMidY - pMidY)/(entity.height/2);
+
+    var absDX = Math.abs(normalX);
+    var absDY = Math.abs(normalY);
+    if (Math.abs(absDX - absDY) < .1) {
+
+        // If the player is approaching from positive X
+        if (normalX < 0) {
+
+            // Set the player x to the right side
+            player.x = entity.x + entity.width;
+
+            // If the player is approaching from negative X
+        } else {
+
+            // Set the player x to the left side
+            player.x = entity.x - player.width;
+        }
+
+        // If the player is approaching from positive Y
+        if (normalY < 0) {
+
+            // Set the player y to the bottom
+            player.y = entity.y + entity.height;
+
+            // If the player is approaching from negative Y
+        } else {
+
+            // Set the player y to the top
+            player.y = entity.y - player.height;
+            grounded = true;
+        }
+
+        // Randomly select a x/y direction to reflect velocity on
+        if (Math.random() < .5) {
+
+            // Reflect the velocity at a reduced rate
+            dx = -dx * entity.restitution;
+
+            // If the objectâ€™s velocity is nearing 0, set it to 0
+            // STICKY_THRESHOLD is set to .0004
+            if (Math.abs(dx) < STICKY_THRESHOLD) {
+                dx = 0;
+            }
+        } else {
+
+            dy = -dy * entity.restitution;
+            if (Math.abs(dy) < STICKY_THRESHOLD) {
+                dy = 0;
+            }
+        }
+
+        // If the object is approaching from the sides
+    } else if (absDX > absDY) {
+
+        // If the player is approaching from positive X
+        if (normalX < 0) {
+            player.x = entity.x + entity.width;
+
+        } else {
+            // If the player is approaching from negative X
+            player.x = entity.x - player.width;
+        }
+
+        // Velocity component
+        dx = -dx * entity.restitution;
+
+        if (Math.abs(dx) < STICKY_THRESHOLD) {
+            dx = 0;
+        }
+
+        // If this collision is coming from the top or bottom more
+    } else {
+
+        // If the player is approaching from positive Y
+        if (normalY < 0) {
+            player.y = entity.y + entity.width;
+
+        } else {
+            // If the player is approaching from negative Y
+            player.y = entity.y - player.height;
+            // grounded = true;
+        }
+
+        // Velocity component
+        dy = -dy * entity.restitution;
+        if (Math.abs(dy) < STICKY_THRESHOLD) {
+            dy = 0;
+        }
     }
 }
 function drawLevel(){
